b031151e932b411475626be99388802a78af80bc
Nureen Mohamad Zaki <s2255686@bioinfmsc5.bio.ed.ac.uk>
2021-10-26 12:57:11 +0100

    Script to do python in shell from BPSM lecture

---
+++ script_lect11.sh
@@ -0,0 +1,20 @@
+# local seq:
+	# first exon: seq start upto/inc 63rd, second exon: 91st to end
+# name of file: plain_genomic_seq.txt
+
+#!/usr/bin/python3
+
+# read the file and store as object
+my_file= open("plain_genomic_seq.txt")
+>>> plain_genomic_seq= myfile.read()
+plaingenome.txt= open("plain_genomic_seq.txt").read()
+
+# split the sequence into coding and non-coding parts
+# write the exons to individual files + two separate files
+# uppercase seq, DNA
+
+
+while open("
+
+
+
